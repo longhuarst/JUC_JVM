0c32ad06ceb5be086d672bf26c5841e09c94abb7
luhuichen <841105197@qq.com>
2020-01-06 05:48:32 +0800

    01 lock

--- src/c01/SaleTicket.java
+++ src/c01/SaleTicket.java
@@ -2,14 +2,27 @@ package c01;
 
 //3 个售票员 卖 40 张票。
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SaleTicket {
 
     int number = 40;
+    Lock l = new ReentrantLock();//可重入锁
 
     void sale(){
-        if (number > 0){
-            System.out.println(Thread.currentThread().getName()+"卖出的最后第"+number+"张票");
-            number--;
+
+
+        l.lock();
+        try {
+            if (number > 0) {
+                System.out.println(Thread.currentThread().getName() + "卖出的最后第" + number + "张票");
+                number--;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            l.unlock();
         }
 
 
